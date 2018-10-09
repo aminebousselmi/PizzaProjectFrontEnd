83983cc84117de11fe8b54ac57ca05796bd420b6
Mouhamed Amine Bousselmi <mboussel@utbm.fr>
2018-10-09 12:12:20 +0200

    test angular

---
+++ src/app/pizza.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Pizza } from './pizza';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PizzaService {
+  
+  URL = "https://lpa2sgadot.herokuapp.com";
+
+  constructor(private httpClient: HttpClient) { 
+     
+  }
+
+  listPizzas():Observable<Pizza[]>{
+    return this.httpClient.get<Pizza[]>(this.URL + '/pizzas.json');
+  }
+}
---
+++ src/app/pizza/pizza.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pizza } from '../pizza';
+
+@Component({
+  selector: 'app-pizza',
+  templateUrl: './pizza.component.html',
+  styleUrls: ['./pizza.component.css']
+})
+export class PizzaComponent implements OnInit {
+
+  @Input() pizza : Pizza
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  /*modifyPizza(){
+      this.pizza.name = "4 fromages";
+  }*/
+
+}
